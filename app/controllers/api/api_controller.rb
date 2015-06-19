311530f0d07f39853348b161e9b0f23d63a74ce6
87maza <87maza@gmail.com>
2015-06-18 17:46:20 -0700

    added word and reddit api

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -5,5 +5,23 @@ module Api
 		def soup
 			render json: Soup.order("RANDOM()").first, except: [:created_at, :updated_at]
 		end
+		def word
+			@wordapi = HTTParty.get"http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=6e479e8bd483a107c100e0844930329e50962240809ce765c"
+			dictionary = {
+					word => @wordapi['word'].capitalize,
+					definition => @wordapi["note"]
+			}
+
+			render json: dictionary
+		end
+		def reddit
+			@redditapi=HTTParty.get"https://www.reddit.com/top.json"
+			top_post = {
+					title => @redditapi['data']['children'][0]['data']['title'],
+					permalink => @redditapi['data']['children'][0]['data']['permalink']
+			}
+			render json:top_post
+		end
+
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "soup", to: "api#soup", as: :soup
+    get "word", to: "api#word", as: :word
+
   end
 end
