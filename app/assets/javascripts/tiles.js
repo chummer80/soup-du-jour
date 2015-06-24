edc57b2c9e7f3da3d8f116e823d602615adcc71b
VALERIE NGAI <vngai808@gmail.com>
2015-06-24 08:08:12 -0700

    tile shuffle added

--- app/assets/javascripts/tiles.js
+++ app/assets/javascripts/tiles.js
@@ -8,6 +8,8 @@ $.getJSON("http://localhost:3000/api/soup", function(response, status, jqXHR){
 $.getJSON("http://localhost:3000/api/weather", function(response, status, jqXHR){
 	$(".weather").find('p').eq(0).html(response.location);
 	$(".weather").find('p').eq(1).html(response.current_temp);
+	$('<img>', {src: response['icon'], class: 'img-responsive'}).appendTo("#weather");
+
 
 });
 
@@ -23,21 +25,51 @@ $.getJSON("http://localhost:3000/api/reddit", function(response, status, jqXHR){
 
 });
 
-$.getJSON("http://localhost:3000/api/yelp", function(response, status, jqXHR){
-	$(".yelp").find('p').eq(0).html(response.bizname);
-	$('<img>', {src: response['rating'], class: 'img-responsive'}).appendTo(".yelp");
-	$('<img>', {src: response['first_img'], class: 'img-responsive'}).appendTo("#yelp");
-	$(".yelp").find('p').eq(1).html(response.comment);
+$.getJSON("http://localhost:3000/api/restaurant", function(response, status, jqXHR){
+	$(".restaurant").find('p').eq(0).html(response.bizname);
+	$('<img>', {src: response['rating'], class: 'img-responsive'}).appendTo(".restaurant");
+	$('<img>', {src: response['first_img'], class: 'img-responsive'}).appendTo("#restaurant");
+	$(".restaurant").find('p').eq(1).html(response.comment);
+
+});
+
+$.getJSON("http://localhost:3000/api/beer", function(response, status, jqXHR){
+	$(".beer").find('p').eq(0).html(response.beer);
+	$(".beer").find('p').eq(1).html(response.description);
+
+});
+
+$.getJSON("http://localhost:3000/api/event", function(response, status, jqXHR){
+	$('<img>', {src: response['event_pic'], class: 'img-responsive'}).appendTo("#event");
+	$(".event").find('p').eq(0).html(response.name);
+
+});
+
+$.getJSON("http://localhost:3000/api/recipe", function(response, status, jqXHR){
+	$('<img>', {src: response['image'], class: 'img-responsive'}).appendTo("#recipe");
+	$(".recipe").find('p').eq(0).html(response.name);
+
+});
+
+$.getJSON("http://localhost:3000/api/news", function(response, status, jqXHR){
+	$('<img>', {src: response['image'], class: 'img-responsive'}).appendTo("#news");
+	$(".news").find('p').eq(0).html(response.title);
 
 });
 // external js: isotope.pkgd.js
 $(document).ready( function() {
 
-  var $grid = $('.grid').isotope({
-    itemSelector: '.grid-item',
-    masonry: {
-      columnWidth: 100
-    }
+$(window).load(function() {
+
+  var $grid = $('.grid').imagesLoaded(function() {
+    $grid.isotope({
+    	itemSelector: '.grid-item',
+	    layoutMode: 'masonry',
+	    masonry: {
+	      columnWidth: 50
+	    }
+
+	});
   });
 
   $grid.on( 'click', '.grid-item', function() {
@@ -45,6 +77,14 @@ $(document).ready( function() {
     $( this ).toggleClass('gigante');
     $grid.isotope('layout');
   });
+
+  $('.shuffle-div').on( 'click', function() {
+  $grid.isotope('shuffle');
+});
+
+ });
   
 })
+
+
 
