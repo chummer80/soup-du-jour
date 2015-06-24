02e08ddf5e171b70c8ab721d8f72120fee2ddc77
87maza <87maza@gmail.com>
2015-06-23 19:26:13 -0700

    yelp restaurant if statment

--- app/helpers/morsels_helper.rb
+++ app/helpers/morsels_helper.rb
@@ -59,9 +59,9 @@ module MorselsHelper
 		rescue
 			restaurant_img = ""
 		end
-
+		 restaurant_img = ""
 		restaurant_morsel_data ={
-			'first_img' => restaurant_img,
+			'first_img' => restaurant_img == '' ? ActionController::Base.new.view_context.image_url("yelp-restaurant.jpg") : restaurant_img,
 			'bizname' => yelp_api_data.raw_data['businesses'][0]['name'],
 			'rating' => yelp_api_data.raw_data['businesses'][0]['rating_img_url'],
 			'comment' => yelp_api_data.raw_data['businesses'][0]['snippet_text'],
