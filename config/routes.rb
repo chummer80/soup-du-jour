7574d05358d20d8453b5b1b3d12974329e0e3703
Mike Chu <mike.c.chu@gmail.com>
2015-06-24 15:53:16 -0700

    add music video morsel

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     get "beer", to: "api#beer", as: :beer
     get "event", to: "api#event", as: :event
     get "video", to: "api#video", as: :video
+    get "musicvideo", to: "api#musicvideo", as: :musicvideo
     get "recipe", to: "api#recipe", as: :recipe
     get "news", to: "api#news", as: :news
     get "trivia", to: "api#trivia", as: :trivia
