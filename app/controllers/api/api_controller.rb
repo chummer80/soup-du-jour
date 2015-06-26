85f63bce40d80008d80793cc678c4f8b6e3559b5
Mike Chu <mike.c.chu@gmail.com>
2015-06-26 01:53:16 -0700

    refactor code for: API routes, API controller actions, and zip code detection

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -3,113 +3,87 @@ module Api
 		include MorselsHelper
 		protect_from_forgery with: :null_session
 
-		def soup
-			render json: MorselsHelper.get_morsel("soup").data
+		def morsel
+			morsel_list = %w(
+				soup
+				word
+				reddit
+				weather
+				restaurant
+				beer
+				event
+				video
+				musicvideo
+				recipe
+				news
+				trivia
+				deal
+				photo
+				view
+				charity
+			)
+			
+			localized_morsel_list = %w(				
+				weather
+				restaurant				
+				event
+			)
+
+			morsel_type = params[:morsel_type]
+
+			if morsel_list.include?(morsel_type)
+				zip_code = localized_morsel_list.include?(morsel_type) ? get_zip() : ""
+				morsel = MorselsHelper.get_morsel(morsel_type, zip_code)
+				render json: morsel.data
+			else
+				render json: {error: "#{morsel_type} is not a valid endpoint"}
+			end
 		end
 
-		def word
-			render json: MorselsHelper.get_morsel("word").data
-		end
-
-		def reddit
-			render json: MorselsHelper.get_morsel("reddit").data
-		end
-
-		def weather
-			# If zip code was supplied as a query parameter, then use it.
-			zip = params[:zip_code] || get_zip(request)
-			render json: MorselsHelper.get_morsel("weather", zip).data
-		end
-
-		def restaurant
-			zip = params[:zip_code] || get_zip(request)
-			render json: MorselsHelper.get_morsel("restaurant", zip).data
-		end
-
-		def beer
-			render json: MorselsHelper.get_morsel("beer").data
-		end
-
-		def event
-			zip = params[:zip_code] || get_zip(request)
-			render json: MorselsHelper.get_morsel("event", zip).data
-		end
-		
-		def video
-			render json: MorselsHelper.get_morsel("video").data
-		end
-		
-		def musicvideo
-			render json: MorselsHelper.get_morsel("musicvideo").data
-		end
-
-		def recipe
-			render json: MorselsHelper.get_morsel("recipe").data
-		end
-
-		def news
-			render json: MorselsHelper.get_morsel("news").data
-		end
-
-		def trivia
-			render json: MorselsHelper.get_morsel("trivia").data
-		end
-
-		def deal
-			render json: MorselsHelper.get_morsel("deal").data
-		end
-
-		def photo
-			render json: MorselsHelper.get_morsel("photo").data
-		end
-
-		def view
-			render json: MorselsHelper.get_morsel("view").data
-		end
-
-		def charity
-			render json: MorselsHelper.get_morsel("charity").data
-		end
-
+	private
 
 		# Try to detect the correct zip code from either the user profile 
 		# or geocoder (IP address detection).
-		def get_zip(request)
-			zip = nil
-
-			# Try to get zip code from logged-in user's profile
-			if logged_in?
-				zip = current_user.zip_code
+		def get_zip()
+			# Option 1: use zip code supplied in the request params
+			if params[:zip_code]
+				return params[:zip_code]
 			end
 
-			# Try to get zip code from temporary session storage
-			if (zip.nil? || zip == "")
-				zip = session[:detected_zip_code]
+			# Option 2: use zip code in the logged-in user's settings
+			if logged_in? && current_user.zip_code.length > 0
+				return current_user.zip_code
 			end
 
-			# If we still don't have the zip code then get it using the geocoder gem.
+			# Option 3: use zip code that was previously detected and stored in session storage
+			if session[:detected_zip_code]
+				return session[:detected_zip_code]
+			end
+
+			# Option 4: detect zip code using Geocoder gem's IP lookup service
 			# But this only works in production because it needs the request to come from a real IP address.
-			if (zip.nil? || zip == "") && Rails.env.production?
+			if Rails.env.production?
 				puts "Detecting zip code."
 				# The location method can sometimes fail because of timeout so put it in this
 				begin
+					# request.location is a method that Geocoder added
 					zip = request.location.postal_code
 					puts "Detected Zip: #{zip}"
 
-					# Store detected zip code in session storage so it doesn't
-					# have to be detected for each morsel API call.
-					session[:detected_zip_code] = zip unless (zip.nil? || zip == "") 
+					# This isn't guaranteed to return a valid zip so we must check it
+					if zip != nil && zip.length > 0
+						# Store detected zip code in session storage so it doesn't
+						# have to be detected for each morsel API call.
+						session[:detected_zip_code] = zip
+						return zip
+					end
 				rescue
 					puts "Zip code could not be detected."
 				end
 			end
 
-			# If we still don't have the zip code then use a default (Beverly Hills)
-			if (zip.nil? || zip == "")
-				zip = "90210"
-			end	
-
-			zip
+			# Option 5: if zip could not be detected then the last resort is to use 90210 as default.
+			return "90210"
 		end
 	end
 end
