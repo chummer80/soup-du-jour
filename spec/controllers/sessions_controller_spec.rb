97c7926ab1d6dd9634c3ca796a482c1a36e5ee0d
87maza <87maza@gmail.com>
2015-06-28 16:05:55 -0700

    added 7 tests for users/sessions ctrlr, need to add a morselsctrlr

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,58 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
 
+  it "new creates a new user" do
+    get :new
+
+    expect(assigns(:user)).to be_a_new(User)
+  end
+
+  it "should return new view for new method" do
+    get :new
+    expect(response).to render_template(:new)
+  end
+
+
+  it "destroys the session when logging a user out" do
+    delete :destroy
+    expect(assigns(:user)).to be_nil
+  end
+
+  it "should not log in user with incorrect password" do
+    FactoryGirl.build(:valid_user).save
+    post :create, {user:
+                       {
+                           username: "test@test.com",
+                           password: "a23df"
+                       }
+                }
+    expect(session[:user_id]).to be_nil
+    expect(response).to render_template(:new)
+  end
+  it "should log in user with correct username and password" do
+    FactoryGirl.build(:valid_user).save
+    post :create, {user:
+                       {
+                           email: "test@test.com",
+                           password: "asdfasdf"
+                       }
+                }
+    expect(session[:user_id]).to be
+    expect(User.find(session[:user_id])).to be
+    expect(response).to redirect_to(root_path)
+  end
+
+
+  # it "Should redirect to users page when logging a user out" do
+  #   FactoryGirl.build(:valid_user).save
+  #   post :create, {user:
+  #                      {
+  #                          email: "test@test.com",
+  #                          password: "asdfasdf",
+  #                      password_confirmation: "asdfasdf"
+  #                      }
+  #               }
+  #   get :destroy
+  #   expect(response).to eq(:destroy_session)
+  #   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
+  it "should return new view for new method" do
+
+    get :new
+
+    expect(response).to render_template(:new)
+  end
+
+  it "new creates a new user" do
+    get :new
+
+    expect(assigns(:user)).to be_a_new(User)
+  end
+
+  # it "redirects to root path after user registers correctly" do
+  #   FactoryGirl.build(:valid_user).save
+  #   post :create, {user:
+  #                      {
+  #                          email: "test@test.com",
+  #                          password: "asdfasdf"
+  #                      }
+  #               }
+  #
+  #   expect(response).to redirect_to(root_path)
+  # end
+
+
 end
